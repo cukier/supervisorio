2db252d88108faf4a34a85881309df4db97425b3
cuki <mauricio@krebs.com.br>
2013-11-24 17:27:22 -0200

    bug fix: corrigido posicionamento das janelas chamdas pelo selecionador

--- src/cuki/frame/Selecionador.java
+++ src/cuki/frame/Selecionador.java
@@ -68,8 +68,10 @@ public class Selecionador extends JFrame {
 			for (Status status : listaStatusPivo) {
 				if (status == null) {
 					Status statusPivo = new Status(pivo);
-					statusPivo.setLocation(getWidth(), statusPivo.getCont()
-							* statusPivo.getHeight());
+					statusPivo.setLocation(getWidth() + statusPivo.getCont()
+							* 30, statusPivo.getCont() * 30);
+					System.out.println(statusPivo.getPivoName() + " : "
+							+ statusPivo.getCont());
 					listaStatusPivo[cont++] = statusPivo;
 					statusPivo.setVisible(true);
 					break;
